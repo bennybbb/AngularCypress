38d49f4ba19096fa6aa3547b048c510c700faa30
benjaminbrachert <benjamin.brachert@valtech.com>
2021-01-07 18:31:31 +0100

    added the cypress stuff

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,19 @@
+describe('When Angular starting page is loaded', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('smoke test', () => {
+    cy.visit('/');
+    cy.contains('AngularCypress app is running!');
+  });
+
+  it('has an app title, shows proper command by default and reacts on command changes', () => {
+    cy.contains('AngularCypress');
+
+    cy.contains('.terminal', 'ng generate component xyz');
+
+    cy.contains('Angular Material').click();
+    cy.contains('.terminal', 'ng add @angular/material');
+  });
+});
